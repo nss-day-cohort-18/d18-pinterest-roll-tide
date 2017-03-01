c34e54a2fbeb2f52cf34d7e46e232d69f601bc54
Nick Nash <nicknash@Nicks-MacBook-Pro-2.local>
2017-03-01 13:35:29 -0600

    added sign in functionality (Ctrl and Factories)

--- app/app.js
+++ app/app.js
@@ -2,10 +2,53 @@
 
 var app = angular.module("Pinterest", ["ngRoute"]);
 
+let isAuth = (AuthFactory) => new Promise ( (resolve, reject) => {
+  // console.log("running isAuth");
+    AuthFactory.isAuthenticated()
+    .then ( (userExists) => {
+    console.log("userExists", userExists);
+        if (userExists){
+      console.log("Authenticated, go ahead.");
+            resolve();
+        }else {
+      console.log("Authentication rejected, go away.");
+            reject();
+        }
+    });
+});
+
+
 app.config(function($routeProvider) {
 	$routeProvider.
+	// Make a splash page
 	when('/', {
+		templateUrl: "partials/login.html",
+		controller: "UserCtrl"
+	}).
+	when('/login', {
+		templateUrl: "partials/login.html",
+		controller: "UserCtrl"
+	}).
+	when('/home', {
 		templateUrl: "partials/home.html",
-		controller: "controllers/initialCtrl.js"
+		controller: "UserCtrl"
+	}).
+	when('/profile', {
+		templateUrl: "partials/profile.html",
+		controller: "UserCtrl"
 	});
+	
+});
+
+
+app.run(($location, FBCreds) =>{
+    let creds = FBCreds;
+    let authConfig = {
+        apiKey: creds.apiKey,
+        authDomain: creds.authDomain,
+        databaseURL: creds.databaseURL
+    };
+
+
+    firebase.initializeApp(authConfig);
 });
