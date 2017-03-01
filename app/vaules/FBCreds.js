c1d5ebcd5cedaf2f75f65d8f96809866815d212a
Dean Smith <Dean Smith>
2017-03-01 09:27:42 -0600

    added fb creds

---
+++ app/vaules/FBCreds.js
@@ -0,0 +1,7 @@
+"use strict";
+
+app.constant("FBCreds", {
+    apiKey: "AIzaSyBYUGtxKn6Dv3MPQi3ikuXaYce8xygTK1Q",
+    authDomain: "pinterest-clone-b3a6b.firebaseapp.com",
+    databaseURL: "https://pinterest-clone-b3a6b.firebaseio.com"
+});    
