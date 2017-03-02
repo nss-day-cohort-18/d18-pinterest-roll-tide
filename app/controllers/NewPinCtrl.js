4be720d70fa5ed6cdcc3989a4c3ecf2bb1faf836
Talbot <talbotlawrence75@gmail.com>
2017-03-02 10:54:58 -0600

    Udating the board and pin controllers

--- app/controllers/NewPinCtrl.js
+++ app/controllers/NewPinCtrl.js
@@ -1,1 +1,26 @@
 "use strict";
+
+app.controller("NewPinCtrl", function($scope, PinFactory, $location, AuthFactory){
+
+	let user = AuthFactory.getUser();
+
+	$scope.btnText = "Create";
+	$scope.btnText = "Cancel";
+	$scope.newText = {};
+
+	$scope.newTask = {
+		uid: "",
+		boardid: "",
+		url: "",
+		title: ""
+	};
+
+	$scope.addNewPin = function(){
+		PinFactory.saveNewPin($scope.newTask)	//ask Dean about his saveNewPin ***and what is newTask
+		.then(function(response){
+			$location.url("");
+		});
+
+		$scope.newTask = {};
+	};
+});
