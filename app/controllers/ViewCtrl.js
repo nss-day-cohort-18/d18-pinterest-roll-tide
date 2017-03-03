bd2f2930d139fc1935e19522ac1751b527e7c102
Talbot <talbotlawrence75@gmail.com>
2017-03-03 09:53:29 -0600

    still working on pins partial

--- app/controllers/ViewCtrl.js
+++ app/controllers/ViewCtrl.js
@@ -1,19 +1,24 @@
 "use strict";
 
-app.controller("ViewCtrl", function($scope, $routeParams, PinFactory, AuthFactory){
-	$scope.pins = [];
+app.controller("ViewCtrl", function($scope, $routeParams, PinFactory, AuthFactory, $window){
+	// $scope.pins = [];
 
-	let user = AuthFactory.getUser();
-	console.log("ViewCtrl user", user);
+	// let user = AuthFactory.getUser();
+	// console.log("ViewCtrl user", user);
 
-	PinFactory.getPinList(user)
-	.then(function(pinCollection) {
-		$scope.pins = pinCollection;
-		console.log("ViewCtrl pinCollection", pinCollection);
+	// PinFactory.getPinList(user)
+	// .then(function(pinCollection) {
+	// 	$scope.pins = pinCollection;
+	// 	console.log("ViewCtrl pinCollection", pinCollection);
 
-		$scope.selectedpin = $scope.pins.filter(function(pin){
-			return pin.id === $routeParams.pinId;
-			console.log("ViewCtrl pin.id", pin.id);
-		})[0];
-	});
+	// 	$scope.selectedpin = $scope.pins.filter(function(pin){
+	// 		return pin.id === $routeParams.pinId;
+	// 		console.log("ViewCtrl pin.id", pin.id);
+	// 	})[0];
+	// });
+
+	$scope.getPin = function(){
+		$window.location.url = "#!/pinView";
+	};
+	
 });
