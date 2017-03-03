e63bba98e162a4d546051995bd99de9c1f9ea404
Dean Smith <Dean Smith>
2017-03-03 11:04:09 -0600

    Adding filter that almost works

--- app/controllers/userDataCtrl.js
+++ app/controllers/userDataCtrl.js
@@ -1,19 +1,22 @@
-
 "use strict";
 /*********************************
 -Get userlistCtrl
 *********************************/
 
-app.controller("UserDataCtrl", function($scope, ItemStorage, SearchTermData, AuthFactory){
+app.controller("UserDataCtrl", function($scope, ItemStorage, SearchTermData, AuthFactory, PinFactory){
     console.log("UserDataCtrl");
 
-
+    $scope.searchText = SearchTermData;
     let user = AuthFactory.getUser();
 
     PinFactory.getBoardList(user)
-  	 .then(function(boardCollection){
-  		$scope.item = boardCollection;
+       .then(function(boardCollection){
+          $scope.item = boardCollection;
+      });
 
     PinFactory.getBoardList(user)
       .then(function(pinCollection){
         $scope.item = pinCollection;
+    });
+
+});
