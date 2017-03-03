8f62cde4a36a90ecdce4d29f34eb17bb591c4515
Talbot <talbotlawrence75@gmail.com>
2017-03-02 21:43:02 -0600

    worked on the viewCtrl and the home.html

--- app/controllers/ViewCtrl.js
+++ app/controllers/ViewCtrl.js
@@ -1,1 +1,19 @@
 "use strict";
+
+app.controller("ViewCtrl", function($scope, $routeParams, PinFactory, AuthFactory){
+	$scope.pins = [];
+
+	let user = AuthFactory.getUser();
+	console.log("ViewCtrl user", user);
+
+	PinFactory.getPinList(user)
+	.then(function(pinCollection) {
+		$scope.pins = pinCollection;
+		console.log("ViewCtrl pinCollection", pinCollection);
+
+		$scope.selectedpin = $scope.pins.filter(function(pin){
+			return pin.id === $routeParams.pinId;
+			console.log("ViewCtrl pin.id", pin.id);
+		})[0];
+	});
+});
